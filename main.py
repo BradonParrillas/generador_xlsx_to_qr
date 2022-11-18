fb5deab4bd82f68e65849f175594a0d2d1ad0918
DanielInstitucional <daniel16parrillas2@gmail.com>
2022-11-18 13:25:54 -0600

    Se corrigio script

--- main.py
+++ main.py
@@ -8,17 +8,28 @@ workbook = openpyxl.load_workbook(path_data)
 sheet_data = workbook.active
 last_row = sheet_data.max_row
 
-# QR
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
-
 # Colors
 fill_color_rgb = (37, 38, 94)
 back_color_rgb = (255, 255, 255)
+
+def generar_qr(data, name, description = ''):
+    # QR
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=50,
+        border=1,
+    )
+
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    qr_img = qr.make_image(fill_color=fill_color_rgb, back_color=back_color_rgb)
+
+    image_file = open(name, 'wb')
+    qr_img.save(image_file)
+    image_file.close()
+
 
 for i in range(2, last_row + 1):
     title = sheet_data.cell(row=i, column=1).value
@@ -27,12 +38,5 @@ for i in range(2, last_row + 1):
 
     qr_name = path_qr_images + title[:-4] + ' - ' + description + '.png'
 
-    qr.add_data(qr_data)
-    qr.make(fit=True)
-
-    qr_img = qr.make_image(fill_color=fill_color_rgb, back_color=back_color_rgb)
-
-    image_file = open(qr_name, 'wb')
-    qr_img.save(image_file)
-    image_file.close()
-
+    generar_qr(qr_data, qr_name, description)
+    print("QR generado: ", title[:-4], description)
